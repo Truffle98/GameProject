274cc587ddfe915f429e872848d42715e75f4366
j03m4r <joeemarr12345@gmail.com>
2022-05-02 21:57:09 -0500

    Moving up anims

--- RPGProject/Assets/Scripts/PlayerMovement.cs
+++ RPGProject/Assets/Scripts/PlayerMovement.cs
@@ -8,10 +8,15 @@ public class PlayerMovement : MonoBehaviour
     private float horizontalInput;
     private float verticalInput;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool movingUp;
+    private bool movingDown;
 
     private void Awake()
     {
+        //Grab references for rigidbody and animator from object
         body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -21,6 +26,37 @@ public class PlayerMovement : MonoBehaviour
 
         body.velocity = new Vector2(horizontalInput * speed, verticalInput * speed);
 
-        
+        if (horizontalInput > .01f)
+        {
+            transform.localScale = new Vector3(-6, 6, 6);
+        }
+        else if (horizontalInput < -.01f)
+        {
+            transform.localScale = Vector3.one * 6;
+        }
+
+        if (verticalInput > .01f)
+        {
+            movingUp = true;
+        }
+        else if (verticalInput == 0)
+        {
+            if (movingUp)
+            {
+                movingUp = false;
+            }
+            else if (movingDown)
+            {
+                movingDown = false;
+            }
+        }
+        else if (verticalInput < -.01f)
+        {
+            movingDown = true;
+        }
+
+        //Set animator parameters
+        anim.SetBool("Running", horizontalInput != 0);
+        anim.SetBool("UpRunning", movingUp);
     }
 }

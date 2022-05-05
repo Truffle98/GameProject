341a9bcd5d20beb168634f4ca8c85b900d11b88c
j03m4r <joeemarr12345@gmail.com>
2022-05-05 00:50:56 -0500

    Item pickup

---
+++ RPGProject/Assets/Scripts/ItemID.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemID : MonoBehaviour
+{
+    public int itemID;
+
+    public int GetItemID()
+    {
+        return itemID;
+    }
+}

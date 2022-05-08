2744d185e835c4dc49c0fe7b11fe170abab017ac
Tommy Fischer <75415271+Truffle98@users.noreply.github.com>
2022-05-07 19:20:38 -0500

    Added Day/Night Cycle

---
+++ RPGProject/Assets/Scripts/DayNightScript.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DayNightScript : MonoBehaviour
+{
+    [SerializeField] private Gradient lightColor;
+    [SerializeField] private UnityEngine.Rendering.Universal.Light2D light;
+
+    private int days;
+    public float time = 50;
+    private bool canChangeDay = true;
+
+    public int GetDays() {
+        return days;
+    }
+
+    public void DayChanged() {
+
+        return;
+    }
+
+    private void Update() {
+        if (time > 500) {
+            time = 0;
+        }
+
+        if ((int)time == 250 && canChangeDay) {
+            
+            canChangeDay = false;
+            DayChanged();
+            days++;
+
+        }
+
+        if ((int) time == 255) {
+            canChangeDay = true;
+        }
+
+        time += Time.deltaTime;
+        light.color = lightColor.Evaluate(time * 0.002f);
+    }
+}

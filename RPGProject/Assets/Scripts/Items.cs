5344595f6975a65f829513f95a85da286ca436f9
j03m4r <joeemarr12345@gmail.com>
2022-05-11 22:04:45 -0500

    Fixed stacking number going to the next line when double digits stacks were reached

--- RPGProject/Assets/Scripts/Items.cs
+++ RPGProject/Assets/Scripts/Items.cs
@@ -7,7 +7,7 @@ public class Items : BaseClass
 {
     protected string[] itemNames = new string[] { "Gold Coin", "Silver Coin", "Copper Coin", "Fireball", "Stick", "Long Sword" };
     //Indices inside the imbeded array represent: 0:damage, 1:stack max, 2:mana, 3:cooldown, 4:item type (-1 = not weapon, 0 = melee, 1 = ranged)
-    protected int[,] itemStats = new int[6, 5] { {0,0,0,0,-1}, {0,100,0,0,-1}, {0,100,0,0,-1}, {10,1,20,100,1}, {0,3,0,0,-1}, {5,1,0,50,0}};
+    protected int[,] itemStats = new int[6, 5] { {0,0,0,0,-1}, {0,100,0,0,-1}, {0,100,0,0,-1}, {10,1,20,100,1}, {0,10,0,0,-1}, {5,1,0,50,0}};
 
     public float GetItemDamage(int itemID)
     {

cacd1ea5c466126a1847a485580536d39c23728c
j03m4r <joeemarr12345@gmail.com>
2022-05-15 23:58:46 -0500

    Changed suckysucky damage

--- RPGProject/Assets/Scripts/Items.cs
+++ RPGProject/Assets/Scripts/Items.cs
@@ -9,7 +9,7 @@ public class Items : BaseClass
     //Indices inside the imbeded array represent (-1 value = nothing): 0:damage, 1:stack max, 2:mana, 3:cooldown, 4:item type (-1 = not weapon, 0 = melee, 1 = ranged),
     //5: special effects (for abilities), 6: armor type (-1 = not armor, 0 = head, 1 = chest, 2 = boots/legs, 3 = ring one, 4 = ring two), 7: armor value
     protected int[,] itemStats = new int[10, 8] { {0,0,0,0,-1,0,-1,0}, {0,100,0,0,-1,0,-1,0}, {0,100,0,0,-1,0,-1,0}, {10,1,20,100,1,0,-1,0}, {0,10,0,0,-1,0,-1,0}, 
-                                                {5,1,0,50,0,0,-1,0}, {5,1,25,125,1,0,-1,0}, {0,1,0,0,-1,0,2,2}, {8,1,15,120,1,0,-1,0}, {1,1,20,200,0,0,-1,0}};
+                                                {5,1,0,50,0,0,-1,0}, {10,1,25,125,1,0,-1,0}, {0,1,0,0,-1,0,2,2}, {8,1,15,120,1,0,-1,0}, {1,1,20,200,0,0,-1,0}};
 
     public float GetItemDamage(int itemID)
     {
